27971381bc7e7be7e299a4b6c7480a2e9ea775a3
Unk0wn1337 <116260492+Unk0wn1337@users.noreply.github.com>
2023-08-16 13:24:37 +0200

    ModalPopUp

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,17 +1,28 @@
 
 $(document).ready(function() {
+      $('section.sec-1').addClass('show-animate');
       let sections = $('section');
       CallIcon();
+      $('.modal-trigger').on('click', function() {
+        let content = $(this).data('content');
+        $('.modal-text').text(content);
+        openModal(); // Modal fadeIn aktiválása
+      });
+
+      $('.close-button').on('click', function() {
+          closeModal(); // Modal fadeOut aktiválása
+      });
+
       $(window).scroll(function() {
         sections.each(function() {
           let top = $(window).scrollTop();
           let offset = $(this).offset().top - 150;
           let height = $(this).outerHeight();
           if (top >= offset && top < offset + height) {
-            $(this).addClass('show-animate');
-          } else {
-            $(this).removeClass('show-animate');
-          }
+              $(this).addClass('show-animate');
+            } else {
+              $(this).removeClass('show-animate');
+            }
         });
       });
     });
@@ -27,5 +38,15 @@ function CallIcon() {
   $(phoneIcon).on("mouseleave", function () {
     $(this).removeClass("hovered");
   });  
-}   
+} 
+
+function openModal() {
+  $('.modal').fadeIn(); // Modal fadeIn aktiválása
+}
+
+function closeModal() {
+  $('.modal').fadeOut(); // Modal fadeOut aktiválása
+}
+ 
+  
     
